137ca3ab84dee27ff520ec5f501750fa931ddf5f
Martyn James <mjames@edx.org>
2014-12-09 16:43:22 -0500

    hook up ui to search

--- common/djangoapps/search/views.py
+++ common/djangoapps/search/views.py
@@ -1,10 +1,12 @@
 from django.utils.translation import ugettext as _
+from django.views.decorators.csrf import csrf_exempt
 
 from manager import SearchEngine
 from django.http import HttpResponse
 import json
 
 
+@csrf_exempt
 def do_search(request, course_id=None):
     results = {
         "error": _("Nothing to search")
@@ -15,10 +17,18 @@ def do_search(request, course_id=None):
         if request.method == 'POST':
             field_dictionary = None
             if course_id:
-                field_dictionary = {"course_id": course_id}
+                field_dictionary = {"course": course_id}
             searcher = SearchEngine.get_search_engine("courseware_index")
             results = searcher.search_string(
                 request.POST["search_string"], field_dictionary=field_dictionary)
+
+            # update the data with the right stuff
+            for result_data in [result["data"] for result in results["results"]]:
+                result_data["url"] = u"/courses/{course_id}/jump_to/{location}".format(
+                        course_id=result_data["course"],
+                        location=result_data["id"],
+                    )
+
             status_code = 200
     except Exception as err:
         results = {
