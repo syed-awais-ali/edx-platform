6690e79744ad054abb1bf578b2a9e72537ba9342
Chris Dodge <cdodge@edx.org>
2014-12-17 19:06:44 -0500

    re-instate the previous xBlock field 'discussion_id' so that we are backwards compatible with any existing data

--- common/lib/xblock/discussion/discussion_forum/discussion_forum.py
+++ common/lib/xblock/discussion/discussion_forum/discussion_forum.py
@@ -1,4 +1,5 @@
 import logging
+from uuid import uuid4
 
 from xblock.core import XBlock
 from xblock.fields import Scope, String
@@ -17,6 +18,9 @@ log = logging.getLogger(__name__)
 @XBlock.needs('discussion')
 class DiscussionXBlock(XBlock):
     """ Provides functionality similar to discussion XModule in inline mode """
+
+    discussion_id = String(scope=Scope.settings, default=lambda: uuid4().hex)
+
     display_name = String(
         display_name="Display Name",
         help="Display name for this module",
@@ -41,13 +45,6 @@ class DiscussionXBlock(XBlock):
         scope=Scope.settings
     )
     sort_key = String(scope=Scope.settings)
-
-    @property
-    def discussion_id(self):
-        """
-        :return: int discussion id
-        """
-        return self.scope_ids.usage_id.block_id
 
     @property
     def course_id(self):
