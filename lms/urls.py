e5702157777cf22450cf426f241fa2c255fc7286
Chris Dodge <cdodge@edx.org>
2015-03-01 01:21:04 -0500

    wip

--- lms/urls.py
+++ lms/urls.py
@@ -556,3 +556,8 @@ urlpatterns += (
     url(r'404', handler404),
     url(r'500', handler500),
 )
+
+if settings.FEATURES.get('ENABLE_NOTIFICATIONS'):
+    urlpatterns += (
+        url(r'^api/', include('edx_notifications.server.api.urls')),
+    )
