6589c0cc07eacfb58d7347b51dfd6fc722c97eff
wajeeha-khalid <wajeeha.khalid@arbisoft.com>
2017-09-27 18:17:25 +0500

    added CorsPostCsrfMiddleware to fix csrf referer issue with scorm xblock

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1171,6 +1171,7 @@ MIDDLEWARE_CLASSES = (
     'openedx.core.djangoapps.cors_csrf.middleware.CorsCSRFMiddleware',
     'openedx.core.djangoapps.cors_csrf.middleware.CsrfCrossDomainCookieMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
 
     'splash.middleware.SplashMiddleware',
 
@@ -2395,6 +2396,7 @@ if FEATURES.get('ENABLE_CORS_HEADERS'):
     CORS_ALLOW_CREDENTIALS = True
     CORS_ORIGIN_WHITELIST = ()
     CORS_ORIGIN_ALLOW_ALL = False
+    CORS_REPLACE_HTTPS_REFERER = True
 
 # Default cache expiration for the cross-domain proxy HTML page.
 # This is a static page that can be iframed into an external page
